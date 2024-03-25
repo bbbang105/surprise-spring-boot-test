83f3aa385ab3885d01b55fad1a2ac3cf9e714592
bbbang105 <2018111366@dgu.ac.kr>
2024-03-26 02:56:39 +0900

    feat : tag 엔터티 추가

---
+++ src/main/java/org/example/surprise_spring_boot_test/entity/Tag.java
@@ -0,0 +1,29 @@
+package org.example.surprise_spring_boot_test.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "tag")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+
+    @Column(name = "tag_uuid")
+    private UUID tagId;
+
+    @Column(name = "tag_name")
+    private String name;
+
+    @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ExhibitionTag> exhibitionTags;
+}
