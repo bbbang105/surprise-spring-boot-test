5efb91626109aa9cbe063650d7f2cd96afb0827c
bbbang105 <2018111366@dgu.ac.kr>
2024-03-26 03:59:24 +0900

    feat : user_tag 레포지토리 추가

---
+++ src/main/java/org/example/surprise_spring_boot_test/repository/UserTagRepository.java
@@ -0,0 +1,10 @@
+package org.example.surprise_spring_boot_test.repository;
+
+import org.example.surprise_spring_boot_test.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserTagRepository extends JpaRepository<User, Long> {
+
+}
