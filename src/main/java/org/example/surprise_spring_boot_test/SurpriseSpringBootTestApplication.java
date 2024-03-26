e576d198fe4d14cd8e83f1ac43791ac060da358b
bbbang105 <2018111366@dgu.ac.kr>
2024-03-26 18:14:40 +0900

    chore : 기타 빌드 파일들 추가

---
+++ src/main/java/org/example/surprise_spring_boot_test/SurpriseSpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.surprise_spring_boot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurpriseSpringBootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurpriseSpringBootTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/surprise_spring_boot_test/SurpriseSpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.surprise_spring_boot_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurpriseSpringBootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
