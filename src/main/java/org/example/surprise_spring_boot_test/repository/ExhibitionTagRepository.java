ca90001397037d8224d0f02cfbb13405a6004327
bbbang105 <2018111366@dgu.ac.kr>
2024-03-26 18:13:20 +0900

    feat : 반환 메서드 추가

--- src/main/java/org/example/surprise_spring_boot_test/repository/ExhibitionTagRepository.java
+++ src/main/java/org/example/surprise_spring_boot_test/repository/ExhibitionTagRepository.java
@@ -4,7 +4,9 @@ import org.example.surprise_spring_boot_test.entity.ExhibitionTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExhibitionTagRepository extends JpaRepository<ExhibitionTag, Long> {
-
+    List<ExhibitionTag> findByExhibitionId(Long exhibitionId);
 }
--- src/main/java/org/example/surprise_spring_boot_test/repository/UserTagRepository.java
+++ src/main/java/org/example/surprise_spring_boot_test/repository/UserTagRepository.java
@@ -1,10 +1,12 @@
 package org.example.surprise_spring_boot_test.repository;
 
-import org.example.surprise_spring_boot_test.entity.User;
+import org.example.surprise_spring_boot_test.entity.UserTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserTagRepository extends JpaRepository<User, Long> {
+import java.util.List;
 
+@Repository
+public interface UserTagRepository extends JpaRepository<UserTag, Long> {
+    List<UserTag> findByUserId(Long userId);
 }
